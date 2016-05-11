5a2368827d67c51a18b394b903ce0a36ea7dc748
Eric Kernfeld <eric.kern13@gmail.com>
2016-05-11 16:41:43 -0700

    made it object oriented. About to do a full run on my desktop and pickle results.

--- TheanoCNN.py
+++ TheanoCNN.py
@@ -97,136 +97,131 @@ class LeNetConvPoolLayer(object):
         self.input = input
 
 class LeNet():
-    def __init__(self, filter_diam = 9, maxpool_size = 2):
-        self.filter_diam = filter_diam
-        self.maxpool_size = maxpool_size
-        return
-
-    def update_image_size(self, original):
-        return int(np.ceil((original - self.filter_diam + 1) / self.maxpool_size))
-
-    def do_conv_pool(self, layer0_input, image_size, batch_size, nkern, nkern_prev):
-        layer0 = LeNetConvPoolLayer(
-                rng,
-                input=layer0_input,
-                image_shape=(batch_size, nkern_prev, image_size[0], image_size[1]),
-                filter_shape=(nkern, nkern_prev, self.filter_diam, self.filter_diam),
-                poolsize=(2, 2)
-                )
-        for i in range(2):
-            image_size[i] = self.update_image_size(image_size[i])
-
-        return layer0, image_size
-
-    def eval_lenet5(self, get_batch, image_size,
-                    learning_rate=0.1, n_batches=20,
-                    nkerns=[20, 50], batch_size=500):
+    def __init__(self, image_size, get_batch, nkerns=[20, 50],
+                 filter_diam = 9, maxpool_size = 2,
+                 learning_rate=0.1, batch_size=500):
         """
         :type learning_rate: float
         :param learning_rate: learning rate used (factor for the stochastic
                               gradient)
 
-        :type n_epochs: int
-        :param n_epochs: maximal number of epochs to run the optimizer
+        :type n_batches: int
+        :param n_batches: maximal number of batches to run the optimizer
 
-        :type next_datum: function
-        :param next_datum: function that returns tuples of the form (image, label)
+        :type get_batch: function
+        :param get_batch: function that returns tuples of the form (images, labels).
+        Its inputs are batch_size and CV_type. See the documentation of
+        AstroImageMunger.get_batch for more details.
 
         :type nkerns: list of ints
         :param nkerns: number of kernels on each layer
         """
+        self.filter_diam = filter_diam
+        self.batch_size = batch_size
+        self.filter_diam = filter_diam
+        self.maxpool_size = maxpool_size
+        self.rng = np.random.RandomState(23455)
+        self.batch_size = batch_size
+        self.get_batch = get_batch
 
-        rng = np.random.RandomState(23455)
-
-        ######################
-        # BUILD MODEL        #
-        ######################
         print('... building the model')
+        #=========  Set up Theano basics  =========
 
-        # Form input of shape (batch_size, 1, image_size[0], image_size[1])
-
-        test_set_x,  test_set_y  = get_batch(batch_size = batch_size, CV_type="test")
-
-        #Feed it to Theano
-        x = T.dtensor4("x")
-        y = T.ivector("y")
-        index = T.lscalar()  # index to a [mini]batch
+        #Symbolic input to Theano
+        self.x = T.dtensor4("x")
+        self.y = T.ivector("y")
 
         #set up two convolutional pooling layers
-        layer0, image_size = self.do_conv_pool(x,             image_size, batch_size, nkern = nkerns[0], nkern_prev = image_size[2])
-        layer1, image_size = self.do_conv_pool(layer0.output, image_size, batch_size, nkern = nkerns[1], nkern_prev = nkerns[0])
-
+        self.layer0, image_size = self.do_conv_pool(self.x,             image_size, nkern = nkerns[0], nkern_prev = image_size[2])
+        self.layer1, image_size = self.do_conv_pool(self.layer0.output, image_size, nkern = nkerns[1], nkern_prev = nkerns[0])
         # the HiddenLayer being fully-connected, it operates on 2D matrices of
         # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
-        layer2_input = layer1.output.flatten(2)
-
+        self.layer2_input = self.layer1.output.flatten(2)
         # construct a fully-connected sigmoidal layer
-        layer2 = HiddenLayer(
+        self.layer2 = HiddenLayer(
             rng,
-            input=layer2_input,
+            input=self.layer2_input,
             n_in=nkerns[1] * image_size[0] * image_size[1],
             n_out=500,
             activation=T.tanh
         )
 
         # classify the values of the fully-connected sigmoidal layer
-        layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
-
-        print("...built layers. Initializing cost function.")
-
+        self.layer3 = LogisticRegression(input=self.layer2.output, n_in=500, n_out=10)
         # the cost we minimize during training is the NLL of the model
-        cost = layer3.negative_log_likelihood(y)
+        batch_template_xy = self.get_batch(batch_size = batch_size, CV_type="any")
 
-        # create a function to compute the mistakes that are made by the model
-        test_model = theano.function(
-            inputs = [],
-            outputs = layer3.errors(y),
-            givens = {x: test_set_x, y: test_set_y}
-        )
+        #=========  Set up Theano equipment specific to training =========
 
+        self.cost = self.layer3.negative_log_likelihood(self.y)
         # create a list of all model parameters to be fit by gradient descent
-        params = layer3.params + layer2.params + layer1.params + layer0.params
-
+        self.params = self.layer3.params + self.layer2.params + self.layer1.params + self.layer0.params
         # create a list of gradients for all model parameters
-        grads = T.grad(cost, params)
-
+        self.grads = T.grad(self.cost, self.params)
         # train_model is a function that updates the model parameters by
         # SGD Since this model has many parameters, it would be tedious to
         # manually create an update rule for each model parameter. We thus
         # create the updates list by automatically looping over all
         # (params[i], grads[i]) pairs.
-        updates = [
+        self.updates = [
             (param_i, param_i - learning_rate * grad_i)
-            for param_i, grad_i in zip(params, grads)
+            for param_i, grad_i in zip(self.params, self.grads)
         ]
-        train_set_x, train_set_y = get_batch(batch_size = batch_size, CV_type="train")
-        train_set_x_T = theano.shared(train_set_x)
-        train_set_y_T = theano.shared(train_set_y)
-        train_model = theano.function(
+        self.train_set_x_T = theano.shared(batch_template_xy[0])
+        self.train_set_y_T = theano.shared(batch_template_xy[1])
+        self.train_model = theano.function(
             [],
-            cost,
-            updates=updates,
-            givens={x:train_set_x_T, y:train_set_y_T}
+            self.cost,
+            updates=self.updates,
+            givens={self.x:self.train_set_x_T, self.y:self.train_set_y_T}
         )
-        # end-snippet-1
 
-        ###############
-        # TRAIN MODEL #
-        ###############
-        print('... training')
+        #=========  Set up Theano equipment specific to testing =========
+        self.test_set_x_T = theano.shared(batch_template_xy[0])
+        self.test_set_y_T = theano.shared(batch_template_xy[1])
+        self.test_model = theano.function(
+            inputs = [],
+            outputs = self.layer3.errors(self.y),
+            givens = {self.x: self.test_set_x_T, self.y: self.test_set_y_T}
+        )
 
+        return
+
+    def train(self, n_batches):
         start_time = timeit.default_timer()
         for i in range(n_batches):
-            train_set_x, train_set_y = get_batch(batch_size = batch_size, CV_type="train")
-            train_set_x_T.set_value(train_set_x)
-            train_set_y_T.set_value(train_set_y)
-            train_model()
-        test_loss = test_model()
-
+            print("Training batch ", i, " of ", n_batches)
+            train_set_x, train_set_y = self.get_batch(batch_size = self.batch_size, CV_type="train")
+            self.train_set_x_T.set_value(train_set_x)
+            self.train_set_y_T.set_value(train_set_y)
+            self.train_model()
         end_time = timeit.default_timer()
-        print('Optimization complete. Test loss is: ', test_loss)
         print(('The code for file ' +
                os.path.split(__file__)[1] +
                ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
+        return
 
-        return(test_loss)
+    def test(self, n_test_batches):
+        test_losses = []
+        for i in range(n_test_batches):
+            test_set_x,  test_set_y  = self.get_batch(batch_size = self.batch_size, CV_type="test")
+            self.test_set_x_T.set_value(test_set_x)
+            self.test_set_y_T.set_value(test_set_y)
+            test_losses.append(self.test_model())
+        return np.mean(test_losses)
+
+
+    def update_image_size(self, original):
+        return int(np.ceil((original - self.filter_diam + 1) / self.maxpool_size))
+
+    def do_conv_pool(self, layer0_input, image_size, nkern, nkern_prev):
+        layer0 = LeNetConvPoolLayer(
+                rng,
+                input=layer0_input,
+                image_shape=(self.batch_size, nkern_prev, image_size[0], image_size[1]),
+                filter_shape=(nkern, nkern_prev, self.filter_diam, self.filter_diam),
+                poolsize=(2, 2)
+                )
+        for i in range(2):
+            image_size[i] = self.update_image_size(image_size[i])
+        return layer0, image_size
