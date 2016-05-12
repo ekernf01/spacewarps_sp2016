1ae1d456ed2c1a3ac7e3d8602d70aeae6df91e6c
Eric Kernfeld <eric.kern13@gmail.com>
2016-05-11 22:48:14 -0700

    got results for gradient boosted trees. Yay!

--- boosted_trees.py
+++ boosted_trees.py
@@ -1,31 +1,68 @@
 import sklearn.metrics
 import sklearn.ensemble
-import matplotlib.pyplot as plt
+import ggplot as gg
+import pandas as pd
 from AstroImageMunger import *
 
 swmunge = AstroImageMunger() 
 # recover features image by image in a pd dataframe
-n_train = 5#int(round(11512 * 9 / 10))
-n_test  = 5#int(round(11512 / 10))
-features_train = np.zeros((n_train, 43))
-features_test  = np.zeros((n_test, 43))
+n_train = int(round(11512 * 9 / 10))
+n_test  = int(round(11512 / 10))
+NUM_FEATURES = 43
+features_train = np.zeros((n_train, NUM_FEATURES))
+features_test  = np.zeros((n_test, NUM_FEATURES))
 labels_train = np.zeros(n_train)
 labels_test  = np.zeros(n_test)
 for i in range(n_train):
     features_train[i, :], labels_train[i] = swmunge.nextExample(datum_type="sextractor", CV_type="train")
 for i in range(n_test):
-    features_test[i, :],  labels_test[i]  = swmunge.nextExample(datum_type="sextractor", CV_type="train")
+    features_test[i, :],  labels_test[i]  = swmunge.nextExample(datum_type="sextractor", CV_type="test")
 
+# =====   testing different max depths   ======
+
+fprs = []
+tprs = []
+depths = []
 for depth in [2, 3, 4, 5]:
+    print "Working on depth " + str(depth)
     #train, test
     trees = sklearn.ensemble.GradientBoostingClassifier(max_depth=depth)
     trees.fit(X=features_train, y=labels_train)
     predictions_test = trees.predict_proba(X=features_test)
-    fpr, tpr, thresh = sklearn.metrics.roc_curve(y_true = labels_test, y_score = predictions_test[:, 0])
-    #plot, save
-    figname = "TPR versus FPR for max_depth " + str(depth)
-    filename = "roc_max_depth_" + str(depth)
-    plt.clf()
-    plt.plot(fpr, tpr)
-    plt.title(figname)
-    plt.savefig("results/" + figname + ".png")
+    fpr, tpr, thresh = sklearn.metrics.roc_curve(y_true = labels_test, y_score = predictions_test[:, 1])
+    fprs.extend(fpr)
+    tprs.extend(tpr)
+    depths.extend(depth * np.ones(len(tpr)))
+
+#plot, save
+depths = [str(d) for d in depths] #turn this to string for categorical colour scheme
+to_plot = pd.DataFrame({"FPR": fprs, "TPR": tprs, "depth": depths})
+p = gg.ggplot(data = to_plot, aesthetics = gg.aes(x = "FPR", y = "TPR", colour = "depth")) + \
+    gg.geom_line() + gg.ggtitle("Tree ROCs by max depth") + \
+    gg.xlab("FPR") + gg.ylab("TPR")
+gg.ggsave(filename="results/tree_rocs_by_depth.png", plot = p)
+
+
+# =====   testing different numbers of trees   ======
+
+fprs = []
+tprs = []
+n_trees = []
+for n_trees in [100, 200, 300, 400, 500]:
+    print "Working on n_trees " + str(n_trees)
+    #train, test
+    trees = sklearn.ensemble.GradientBoostingClassifier(max_depth=5, n_estimators = n_trees)
+    trees.fit(X=features_train, y=labels_train)
+    predictions_test = trees.predict_proba(X=features_test)
+    fpr, tpr, thresh = sklearn.metrics.roc_curve(y_true = labels_test, y_score = predictions_test[:, 1])
+    fprs.extend(fpr)
+    tprs.extend(tpr)
+    n_trees.extend(n_trees * np.ones(len(tpr)))
+
+#plot, save
+n_trees = [str(d) for d in n_trees] #turn this to string for categorical colour scheme
+to_plot = pd.DataFrame({"FPR": fprs, "TPR": tprs, "n_trees": n_trees})
+p = gg.ggplot(data = to_plot, aesthetics = gg.aes(x = "FPR", y = "TPR", colour = "n_trees")) + \
+    gg.geom_line() + gg.ggtitle("Tree ROCs by number of trees") + \
+    gg.xlab("FPR") + gg.ylab("TPR")
+gg.ggsave(filename="results/tree_rocs_by_n_trees.png", plot = p)
